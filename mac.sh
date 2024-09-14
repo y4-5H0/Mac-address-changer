0ed108257cf3e18b0711eda4af7730d3cd8dff13
Nintuto <160359868+n1n7u70@users.noreply.github.com>
2024-09-15 01:04:02 +0600

    Add files via upload

--- mac.sh
+++ mac.sh
@@ -3,6 +3,8 @@ macchanger -l > maclist.txt
 ouimac=$(shuf -n 1 maclist.txt | awk'({print$3}')
 uaamac=$(printf'%02x:'%02x:'%02x' $[RANDOM%256] $[RANDOM%256] $[RANDOM%256])
 mac="$ouimac:$uaamac"
+#if you want to change the mac address of wlan0 you need to change the eth0 to wlan0.
 ifconfig eth0 down
-ifcong= eth0 hw ether $mac
+ifcong eth0 hw ether $mac
 ifconfig eth0 up
+
