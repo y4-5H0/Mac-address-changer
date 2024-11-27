7faf4459bd19834aaa94fda28f29fe8c77eb9fb0
n1n7u70 <nintuto00@gmail.com>
2024-11-27 18:45:30 +0600

    Fix shebang line and update MAC address change command in mac.sh

--- mac.sh
+++ mac.sh
@@ -1,10 +1,10 @@
-#!bin/bash
+#!/bin/bash
 macchanger -l > maclist.txt
-ouimac=$(shuf -n 1 maclist.txt | awk'({print$3}')
-uaamac=$(printf'%02x:'%02x:'%02x' $[RANDOM%256] $[RANDOM%256] $[RANDOM%256])
-mac="$ouimac:$uaamac"
+ouimac=$(shuf -n 1 maclist.txt | awk '{print$3}')
+uaamac=$(printf '%02x:''%02x:''%02x' $[RANDOM%256] $[RANDOM%256] $[RANDOM%256])
 #if you want to change the mac address of wlan0 you need to change the eth0 to wlan0.
 ifconfig eth0 down
-ifcong eth0 hw ether $mac
+macchanger -m "$ouimac:$uaamac" eth0
 ifconfig eth0 up
+
 
